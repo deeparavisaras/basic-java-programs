e70b79f5c4b072f4d8074247261aa4cabf609e66
deepa <deeparavikalambur@gmail.com>
2022-07-09 06:39:17 -0700

    adding my first program

---
+++ addtion.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class addition{
+public static int addnumbers(int num1,int num2){
+	int sum=num1+num2;
+	return sum;
+	}
+	public static void main(String[]args){
+	int w=addnumbers(2,4);
+	System.out.println(w);	
+	}
+}
+
