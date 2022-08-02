3f1ef0366678c112cfc790468203ee455224492a
deepa <deeparavikalambur@gmail.com>
2022-08-02 20:02:03 +0530

    changes made

--- 1 aug 2022/main.java
+++ 1 aug 2022/main.java
@@ -1,23 +1,34 @@
-class headmaster{
+class headmaster{ //parent
 	void func(){
 		System.out.println("headmaster");
 	}
 }
-class student{
+class student{ //child
+	
+	//static object for parent class
 	static headmaster staticobject=new headmaster();
-	String name;
+	
+	//static method
 	static void method(){
 		System.out.println("static");
 	}
 }
 class mainclass{
+	
+	//main method
 	public static void main(String[]arguments){
 	System.out.println("main class");
 	
+	//static method/object can be accessed using class name
+	//eg: <classname>.<static method>
+	student.method();
+	
+	//static method/object can be accessed using class name
+	//eg: <classname>.<static object>
+	//calling the function in parent class using the static object
+	//eg: <classname>.<static object>.methodname()
 	student.staticobject.func();
 	
-	
-	student.method();
 }
 }
 	
--- 31 july 2022/programmer.java
+++ 31 july 2022/programmer.java
@@ -4,7 +4,7 @@ class employee{
 class programmer extends employee{
 	int bonus=10000;
 	public static void main(String[]args){
-		programmer p = new programmer();
+	    programmer p = new programmer();
 		System.out.println("programmer salary is:"+p.salary);
 		System.out.println("bonus of programmer is:"+p.bonus);
 	}
